442d8b0cb101955c9c634cd5dfc154406c60947b
mobin kasiri <m.kasiri@sibitec.com>
2024-10-26 15:40:31 +0330

    remove form control wrapper in select component and release new version

--- src/components/common/select/index.tsx
+++ src/components/common/select/index.tsx
@@ -1,6 +1,5 @@
 import { CacheProvider } from "@emotion/react";
 import {
-  FormControl,
   FormControlProps,
   MenuItem,
   TextFieldProps,
@@ -25,29 +24,27 @@ const Select = ({ label, array, onSelectChange, ...rest }: mySelectProps) => {
   return (
     <CacheProvider value={cacheRtl}>
       <ThemeRegistry>
-        <FormControl fullWidth>
-          <TextField
-            select
-            sx={{ fontFamily: fontName }}
-            label={label}
-            onChange={onSelectChange}
-            {...rest}
-          >
-            {array?.map((e) => {
-              if (e?.label) {
-                return (
-                  <MenuItem
-                    sx={{ fontFamily: fontName }}
-                    key={e.value}
-                    value={e.value}
-                  >
-                    {e.label}
-                  </MenuItem>
-                );
-              } else return null;
-            })}
-          </TextField>
-        </FormControl>
+        <TextField
+          select
+          sx={{ fontFamily: fontName }}
+          label={label}
+          onChange={onSelectChange}
+          {...rest}
+        >
+          {array?.map((e) => {
+            if (e?.label) {
+              return (
+                <MenuItem
+                  sx={{ fontFamily: fontName }}
+                  key={e.value}
+                  value={e.value}
+                >
+                  {e.label}
+                </MenuItem>
+              );
+            } else return null;
+          })}
+        </TextField>
       </ThemeRegistry>
     </CacheProvider>
   );
